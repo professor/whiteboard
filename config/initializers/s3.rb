9db76ecb09909d318b03179829f4958dc2e7b95c
Todd Sedano <tsedano@avila.local>
2010-06-08 12:25:41 -0400

    removed reference to amazon s3

---
+++ config/initializers/s3.rb
@@ -0,0 +1,5 @@
+AWS::S3::Base.establish_connection!(
+  :access_key_id     => ENV['S3_KEY'] || 'thedefaultkey',
+  :secret_access_key => ENV['S3_SECRET'] || 'thedefaultsecret',
+  :bucket            => ENV['S3_BUCKET'] || 'thedefaultbucket'
+)
