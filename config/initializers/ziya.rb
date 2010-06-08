59490e7e99164145731e75e7211c83f3c929c276
Todd Sedano <tsedano@avila.local>
2010-06-08 14:19:00 -0400

    working on .gems file

--- config/initializers/ziya.rb
+++
@@ -1,9 +0,0 @@
-
-# Pull in the ZiYa gem framework
-gem "ziya", "~> 2.0.0"
-require 'ziya'
-
-# Initializes the ZiYa Framework
-Ziya.initialize( :logger      => RAILS_DEFAULT_LOGGER,
-                 :helpers_dir => File.join( File.dirname(__FILE__), %w[.. .. app helpers ziya] ),
-                 :themes_dir  => File.join( File.dirname(__FILE__), %w[.. .. public charts themes]) )
