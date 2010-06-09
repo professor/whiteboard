67fae5fc0f8c738100c7c494f061dc091c33edbb
Todd Sedano <tsedano@avila.local>
2010-06-09 17:23:53 -0400

    trying to get bundler to work with heroku

--- config/preinitializer.rb
+++ config/preinitializer.rb
@@ -1,21 +1,23 @@
 begin
+  # Set up load paths for the locked set of pre-resolved gems.
+  require File.expand_path('../../.bundle/environment', __FILE__)
+rescue LoadError
+  # Fall back on trying to resolve out of already-installed gems at runtime.
   require "rubygems"
   require "bundler"
-rescue LoadError
-  raise "Could not load the bundler gem. Install it with `gem install bundler`."
-end
 
-#if Gem::Version.new(Bundler::VERSION) <= Gem::Version.new("0.9.24")
-if Gem::Version.new(Bundler::VERSION) <= Gem::Version.new("0.9")
-  raise RuntimeError, "Your bundler version is too old." +
-   "Run `gem install bundler` to upgrade."
-end
+  if Gem::Version.new(Bundler::VERSION) <= Gem::Version.new("0.9.5")
+    raise RuntimeError,
+      "Your bundler version is incompatible with Rails 2.3 and an unlocked bundle.\n" +
+      "Run `gem install bundler` to upgrade or `bundle lock` to lock."
+  end
 
-begin
-  # Set up load paths for all bundled gems
-  ENV["BUNDLE_GEMFILE"] = File.expand_path("../../Gemfile", __FILE__)
-  Bundler.setup
-rescue Bundler::GemNotFound
-  raise RuntimeError, "Bundler couldn't find some gems." +
-    "Did you run `bundle install`?"
+  begin
+    # Set up load paths for all bundled gems
+    Bundler.setup
+    # Require all gems not in a group
+    Bundler.require :default
+  rescue Bundler::GemNotFound
+    raise Bundler::GemNotFound, "Bundler couldn't find some gems. Did you run `bundle install`?"
+  end
 end
