d565fbce98e332245510b12aeea2769ea79f0dcf
Todd Sedano <tsedano@avila.local>
2010-06-08 22:30:09 -0400

    working on authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,12 +12,10 @@ class User < ActiveRecord::Base
 #  validates_presence_of     :login
   validates_length_of       :login,    :within => 3..40
   validates_uniqueness_of   :login,    :case_sensitive => false, :allow_nil => true
-  validates_format_of       :login,    :with => RE_LOGIN_OK, :message => MSG_LOGIN_BAD
 
 #  validates_presence_of     :email
   validates_length_of       :email,    :within => 6..100 #r@a.wk
 #  validates_uniqueness_of   :email,    :case_sensitive => false
-  validates_format_of       :email,    :with => RE_EMAIL_OK, :message => MSG_EMAIL_BAD
 
   validates_uniqueness_of   :webiso_account,    :case_sensitive => false, :allow_nil => true
 
