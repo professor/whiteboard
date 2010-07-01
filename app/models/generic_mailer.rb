1cf7e16e48e5a450697ae6b3ab1d38c24e09a220
Todd Sedano <tsedano@avila.local>
2010-07-01 10:08:10 -0700

    Trying gmail smtp again

--- app/models/generic_mailer.rb
+++ app/models/generic_mailer.rb
@@ -4,7 +4,8 @@ class GenericMailer < ActionMailer::Base
   def email(options = {})
     subject    options[:subject]
     recipients options[:to]
-    from       options[:from] || ENV['GMAIL_SMTP_USER'] || 'scotty.dog@sv.cmu.edu'
+#    from       options[:from] || ENV['GMAIL_SMTP_USER'] || 'scotty.dog@sv.cmu.edu'
+    from       options[:from] || 'scotty.dog@sv.cmu.edu'
     cc         options[:cc]
     bcc        options[:bcc]
     sent_on    Time.now
