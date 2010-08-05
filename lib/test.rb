ba57a14e0c9031a0ea76cd184c3d1ea2068b9698
Todd Sedano <tsedano@avila.local>
2010-08-04 20:14:31 -0700

    Refactoring effort report (names and flow)

--- lib/test.rb
+++ lib/test.rb
@@ -2,4 +2,7 @@ require 'rails'
 
 a = ActiveRecord::Base.connection.execute("select distinct masters_program from people")
 
+#= ActiveRecord::Base.connection.execute("UPDATE effort_log_line_items SET course_id = 55 WHERE course_id = 66;")
+
+
 puts "hello"
