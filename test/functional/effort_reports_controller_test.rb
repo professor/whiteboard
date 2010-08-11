6fecbe560ec834ffc8381d073a47d0dc1b73fc73
Todd Sedano <tsedano@avila.local>
2010-08-11 08:02:52 -0700

    removing unnecessary javascript references

---
+++ test/functional/effort_reports_controller_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class EffortReportsControllerTest < ActionController::TestCase
+
+
+  def test_campus_semester_as_student
+    login_as :student_sam
+    get :campus_semester
+    assert_response :success
+
+    #assert that the number of entries in the student drop down is two or less
+
+  end
+
+  def test_should_get_index_without_user
+    get :index
+    assert_redirected_to login_google_url
+  end
+#
+
+end
