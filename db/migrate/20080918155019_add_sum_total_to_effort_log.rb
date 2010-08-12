78d9570d956067967de7fd020453f794d456cc2b
Todd Sedano <tsedano@avila.local>
2010-08-12 15:31:07 -0700

    Preparing to put onto github

--- db/migrate/20080918155019_add_sum_total_to_effort_log.rb
+++
@@ -1,9 +0,0 @@
-class AddSumTotalToEffortLog < ActiveRecord::Migration
-  def self.up
-    add_column :effort_logs, :sum, :float
-  end
-
-  def self.down
-    remove_column :effort_logs, :sum
-  end
-end
