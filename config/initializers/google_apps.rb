52900de30c04fd84ac8cf0b50b597f3e6474260b
Todd Sedano <tsedano@avila.local>
2010-08-16 11:21:49 -0700

    trying to debug build machine

--- config/initializers/google_apps.rb
+++ config/initializers/google_apps.rb
@@ -10,8 +10,6 @@ require 'gappsprovisioning/provisioningapi'
 include GAppsProvisioning
 def google_apps_connection
   @google_apps_connection ||= ProvisioningApi.new(GOOGLE_USERNAME, GOOGLE_PASSWORD)
-  puts "GOOGLE_USERNAME #{GOOGLE_USERNAME}"
-  puts "GOOGLE_PASSWORD #{GOOGLE_PASSWORD}"
 rescue
   Rails.logger.debug "had to rescue (ie reconnect) google apps"
   Rails.logger.info "had to rescue (ie reconnect) google apps"
