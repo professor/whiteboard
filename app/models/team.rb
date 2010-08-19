792c6e2b3e29b941ab6348557c986393f73a033d
Todd Sedano <tsedano@avila.local>
2010-08-18 22:12:22 -0700

    Bug fix: why is delayed job not working on team emails

--- app/models/team.rb
+++ app/models/team.rb
@@ -31,7 +31,8 @@ class Team < ActiveRecord::Base
     self.updating_email = true
     logger.debug("team.before_save() executed")
 #    update_google_mailing_list(self.email, self.old_email, self.id)
-    self.send_later(:update_google_mailing_list, self.email, self.old_email, self.id)
+#    self.send_later(:update_google_mailing_list, self.email, self.old_email, self.id)
+    self.delay(:update_google_mailing_list, self.email, self.old_email, self.id)
     self.email = self.email.sub('@west.cmu.edu','@sv.cmu.edu')
   end
 
