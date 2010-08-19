641042d128b048c561190659d5b235b57a3e2b0c
Todd Sedano <tsedano@avila.local>
2010-08-18 20:21:58 -0700

    Bug fix: allow ajax to work with server for adding people to a team

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -14,7 +14,11 @@ class PeopleController < ApplicationController
   # GET /people.xml
   def index
     if params[:search]
-          @people = Person.find(:all, :conditions => ['human_name LIKE ?', "%#{params[:search]}%"])
+      if development?
+        @people = Person.find(:all, :conditions => ['human_name LIKE ?', "%#{params[:search]}%"])
+      else
+        @people = Person.find(:all, :conditions => ['human_name ILIKE ?', "%#{params[:search]}%"])
+      end
     else
           @people = Person.find(:all, :conditions => ['is_active IS TRUE'],  :order => "first_name ASC, last_name ASC")
     end
