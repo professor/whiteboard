b3dc6a01fb3c7f2d1fc6a9f2b581d74098093b3b
Todd Sedano <tsedano@avila.local>
2010-09-07 13:01:41 -0700

    Adding specs to suggestion controller

---
+++ spec/controllers/suggestion_controller_spec.rb
@@ -0,0 +1,92 @@
+require 'spec_helper'
+
+describe SuggestionsController do
+  integrate_views
+
+  describe "NEW suggestion" do
+    it "remembers the HTTP referer" do
+       get :new
+       
+       response.should be_success
+    end
+    it "prompts for an email address if the user is not logged in" do
+       get :new
+       response.should have_tag("input", :id => "suggestion_email")
+    end
+
+  end
+
+  describe "POST suggestion" do
+
+    describe "success" do
+
+# Michael Hartl's way
+#      before(:each) do
+#        @attr = { :comment => "This is a suggestion", :page => "http://rails.sv.cmu.edu",
+#        :email => "" }
+#        @suggestion = Factory(:suggestion, @attr)
+#        Suggestion.stub!(:new).and_return(@suggestion)
+#        @suggestion.should_receive(:save).and_return(true)
+#      end
+
+
+#Rspec books way
+# Repsec start
+      let (:suggestion) { mock_model(Suggestion).as_null_object }
+
+      before do
+        @attr = { :comment => "This is a suggestion", :page => "http://rails.sv.cmu.edu",
+                  :email => "" }        
+        Suggestion.stub(:new).and_return(suggestion)
+        Suggestion.stub(:save).and_return(suggestion)
+      end
+      
+
+      it "creates a new suggestion" do
+#        Suggestion.should_receive(:new).with_options("page" => @attr[:page]).and_return(suggestion)
+        post :create, :suggestion => @attr 
+      end
+#rspec book end
+
+      it "remembers the user id if the user is logged in"
+
+      it "emails Todd that a suggestion has been created"
+
+      it "sets a flash[:notice] message" do
+        post :create
+        flash[:notice].should == "Thank you for your suggestion"
+      end
+
+      it "redirects the user back to the page they came from" do
+        post :create, @attr
+        response.should redirect_to(@attr[:page])
+      end
+
+    end
+
+  end
+
+
+#  describe "Create a suggestion" do
+#    it "should work when the user is not logged in" do
+#      get 'new'
+#      response.should be_success
+#
+#    end
+#
+#    it "should work when the user is logged in "
+#
+#    it "should remember the url of the page the suggestion was made on"
+#
+#    it "should email Todd that a suggestion was created"
+#  end
+#
+#  describe "GET 'index'" do
+#    it "should be successful" do
+#      get 'index'
+#      response.should be_success
+#    end
+#  end
+
+end
+
