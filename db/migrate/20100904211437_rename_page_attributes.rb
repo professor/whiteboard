bbc0b4e78b5b773c4ed9481f40a990af0c5fbc5c
Todd Sedano <tsedano@avila.local>
2010-09-07 12:59:14 -0700

    Updating pages

---
+++ db/migrate/20100904211437_rename_page_attributes.rb
@@ -0,0 +1,18 @@
+class RenamePageAttributes < ActiveRecord::Migration
+  def self.up
+    rename_column :pages, :label, :title
+    add_column :pages, :updated_by_user_id, :integer
+    add_column :pages, :version, :integer
+    add_column :pages, :version_comments, :string
+    add_column :pages, :faculty_notes, :string
+  end
+
+  def self.down
+    rename_column :pages, :title, :label
+    remove_column :pages, :updated_by_user_id
+    remove_column :pages, :version
+    remove_column :pages, :version_comments
+    remove_column :pages, :faculty_notes
+  end
+  
+end
