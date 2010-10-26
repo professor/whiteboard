26a485b586be3c55b8175e5fc3517985cd1f1a77
Todd Sedano <tsedano@avila.local>
2010-10-25 17:02:02 -0700

    Updating with decent starting data

--- db/seeds.rb
+++ db/seeds.rb
@@ -64,6 +64,7 @@ Factory.define :awe_smith, :parent => :person do |p|
   p.last_name "Smith"
   p.human_name "Awe Smith"
   p.image_uri "/images/mascot.jpg"
+  p.email "awe.smith@sv.cmu.edu"
   p.webiso_account "awesm@andrew.cmu.edu"
 end
 
@@ -78,6 +79,7 @@ Factory.define :betty_ross, :parent => :person do |p|
   p.last_name "Ross"
   p.human_name "Betty Ross"
   p.image_uri "/images/mascot.jpg"
+  p.email "betty.ross@sv.cmu.edu"
   p.webiso_account "bross@andrew.cmu.edu"
 end
 
@@ -92,14 +94,35 @@ Factory.define :charlie_moss, :parent => :person do |p|
   p.last_name "Moss"
   p.human_name "Charlie Moss"
   p.image_uri "/images/mascot.jpg"
+  p.email "charlie.moss@sv.cmu.edu"
   p.webiso_account "cmoss@andrew.cmu.edu"
 end
 
 
 
+Factory.define :architecture, :class => Course do |c|
+ c.name "Architecture"
+ c.number "96-705"
+ c.semester "Summer"
+ c.mini "Both"
+ c.year "2008"
+end
+
+
+Factory.define :team_triumphant, :class => Team do |t|
+ t.name "Team Triumphant"
+ t.email "triumphant@sv.cmu.edu"
+ t.tigris_space "http://triumphant.tigris.org/servlets/ProjectDocumentList"
+ t.twiki_space "http://info.sv.cmu.edu/twiki/bin/view/Graffiti/WebHome"
+ t.person_name "Awe Smith"
+ t.person_name2 "Betty Ross"
+ t.person_name3 "Charlie Moss"
+end
+
+
 
 Factory.create(:todd)
-Factory.create(:martin)
+martin = Factory.create(:martin)
 Factory.create(:ed)
 Factory.create(:chris)
 Factory.create(:howard)
@@ -107,5 +130,8 @@ Factory.create(:awe_smith)
 Factory.create(:betty_ross)
 Factory.create(:charlie_moss)
 
+architecture = Factory.create(:architecture)
 
+
+Factory.create(:team_triumphant, :primary_faculty_id => martin.id, :course_id=> architecture)
 
