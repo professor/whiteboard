e3bcf9ab738622cbbc9ea2d309824d24a352c30c
ianzhang <ian.zhang@sv.cmu.edu>
2010-10-28 22:34:02 -0700

    added a new branch and added minor test case on welcome_controller to check the sending pull request

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -16,5 +16,12 @@ describe WelcomeController do
     end
   end
 
+  describe "GET 'config'" do
+    it "should be successful" do
+      get 'config'
+      response.should be_success
+    end
+  end
+
 end
 
