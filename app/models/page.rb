947bfe63e705cc41a0dfef16e69da3266c4da49f
Todd Sedano <tsedano@avila.local>
2010-10-28 21:49:56 -0700

    Display who made last modification and when.

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,6 +2,8 @@ class Page < ActiveRecord::Base
     validates_presence_of :title
     validates_presence_of :updated_by_user_id
 
+    belongs_to :updated_by, :class_name=>'User', :foreign_key => 'updated_by_user_id'
+
 #    acts_as_versioned  :table_name => 'page_versions'
 
     belongs_to :course
