857da2999c7660c71c32cbf8ca46afb18e838811
Alan Mak <alan.mak@sv.cmu.edu>
2010-11-06 18:32:24 -0700

    Fix bug ID 19

--- app/controllers/effort_logs_controller.rb
+++ app/controllers/effort_logs_controller.rb
@@ -345,7 +345,6 @@ class EffortLogsController < ApplicationController
   def effort_for_unregistered_courses
     @error_effort_logs_users = EffortLog.users_with_effort_against_unregistered_courses()
     tmp = EffortLog.users_with_effort_against_unregistered_courses()
-    puts "Hello"
     respond_to do |format|
       format.html
       format.xml  { render :xml => @error_effort_logs_users }
