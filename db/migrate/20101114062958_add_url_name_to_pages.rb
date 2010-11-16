96e84560c325119fe8758192379e33b812d0f80c
Todd Sedano <tsedano@avila.local>
2010-11-16 15:40:30 -0800

    named pages works for /ppm

---
+++ db/migrate/20101114062958_add_url_name_to_pages.rb
@@ -0,0 +1,11 @@
+class AddUrlNameToPages < ActiveRecord::Migration
+  def self.up
+    add_column :pages, :url, :string
+    add_index :pages, :url, :unique => true
+  end
+
+  def self.down
+    remove_index :pages, :url
+    remove_column :pages, :url
+  end
+end
