df492ec6075639ffa70c6848c7b72f5b85253bb6
Todd Sedano <tsedano@avila.local>
2010-11-18 16:46:10 -0800

    Made crypted password wider

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101116234544) do
+ActiveRecord::Schema.define(:version => 20101119000356) do
 
   create_table "course_numbers", :force => true do |t|
     t.string   "name"
