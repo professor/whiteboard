b2ea45c5ffa1f354ce34e6048b625693031f1ec4
Todd Sedano <tsedano@avila.local>
2010-12-13 17:57:59 -0800

    Adding in view tests

---
+++ spec/views/pages/edit.html.erb_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe "pages/edit.html.erb" do
+  before(:each) do
+    UserSession.create(Factory(:faculty_frank))
+    @page = assigns[:page] = stub_model(Page,
+      :new_record? => false
+    )
+    assigns[:courses] =  [
+      stub_model(Course),
+      stub_model(Course)
+    ]    
+#rspec 2
+#    @page = assign(:page, stub_model(Page,
+#      :new_record? => false
+#    ))
+  end
+
+  it "renders the edit page form" do
+    render
+
+    response.should have_tag("form", :action => page_path(@page), :method => "post")
+#rspec 2
+#    rendered.should have_selector("form", :action => page_path(@page), :method => "post")
+  end
+end
