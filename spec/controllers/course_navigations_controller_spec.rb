b27422621865abe0d900f9022482552904322836
Todd Sedano <tsedano@avila.local>
2011-03-20 12:24:11 -0700

    New spec

---
+++ spec/controllers/course_navigations_controller_spec.rb
@@ -0,0 +1,36 @@
+require 'spec-helper'
+
+describe CourseNavigationsController do
+
+  let(:course) { Factory(:course) }
+
+  context "any user can" do
+    before do
+      UserSession.create(Factory(:student_sam))
+    end
+
+    describe "GET show" do
+      before do
+        get :show, :id => course.to_param
+      end
+
+      specify { assigns(:course).should_not be_nil }
+      specify { assigns(:pages).should_not be_nil }
+
+    end
+
+  end
+
+#  context "any faculty can" do
+#    before do
+#      UserSession.create(Factory(:faculty_frank))
+#    end
+#
+#    describe "GET reposition" do
+#
+#      it ''
+#
+#    end
+#  end
+
+end
