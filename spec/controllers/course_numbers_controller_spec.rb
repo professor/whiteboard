c69e2b8b1f24c9cee675606f3c49e6051042ff59
Todd Sedano <tsedano@avila.local>
2011-03-20 12:37:44 -0700

    Refactored CourseNumbers

---
+++ spec/controllers/course_numbers_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe CourseNumbersController do
+
+  let(:course) { Factory(:course) }
+
+  context "any user can" do
+    before do
+      UserSession.create(Factory(:student_sam))
+    end
+
+    describe "GET index" do
+      before do
+        get :index
+      end
+
+      specify { assigns(:courses).should_not be_nil }
+    end
+
+  end
+
+end
