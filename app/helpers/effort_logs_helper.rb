3beb5096a8c3abff2bdf23940308828f2c6b6bf0
Todd Sedano <todd.sedano@sv.cmu.edu>
2011-07-03 11:13:29 -0700

    Refactored to use _form helper

--- app/helpers/effort_logs_helper.rb
+++ app/helpers/effort_logs_helper.rb
@@ -1,18 +1,4 @@
 module EffortLogsHelper
-    
-  def add_line_item_link(name)
-    link_to name,
-      :url => {:action => 'new_effort_log_line_item', :effort_log_id => @effort_log.id },
-      :remote => true,
-      :update => 'add_line_item',
-      :position => :before
-  end
-  
-  def add_time_sheet_line_item_link(name)
-    link_to_function name do |page|
-      page.insert_html :before, :add_line_item, :partial => 'time_sheet_line_item', :object => EffortLogLineItem.new, :local=>@courses
-    end
-  end
 
   def show_week_of_year_text
     "Week " + @effort_log.week_number + " of " + @effort_log.year 
@@ -26,12 +12,5 @@ module EffortLogsHelper
      @today_column == day_of_week ? "<td class='today'>".html_safe : "<td>".html_safe
   end
   
-#  def td_class_is_today(day_of_week)
-#    if @today_column == day_of_week  
-#      return "<td class='today'>" 
-#    else return "<td>" 
-#    end
-#  end
- 
   
 end
