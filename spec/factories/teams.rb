c8539013cb734a84c142d15f0cedd236000277ae
Todd Sedano <todd.sedano@sv.cmu.edu>
2011-07-07 15:05:08 -0700

    .

--- spec/factories/teams.rb
+++ spec/factories/teams.rb
@@ -4,8 +4,6 @@ Factory.define :team_triumphant, :parent => :team do |t|
  t.email "triumphant@sv.cmu.edu"
  t.tigris_space "http://triumphant.tigris.org/servlets/ProjectDocumentList"
  t.twiki_space "http://info.sv.cmu.edu/twiki/bin/view/Graffiti/WebHome"
- t.person_name "Admin Andy"
- t.person_name2 "Faculty Frank"
- t.person_name3 "Student Sam"
  t.association :course, :factory => :course
+ t.after_create { |t| Factory(:student_sam, :team => t)}
 end
