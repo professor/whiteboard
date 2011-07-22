ec716b63354d928add5a9a608349050bdae70343
Chris Jensen <christophercjensen@gmail.com>
2011-07-22 13:53:12 -0700

    updated assignement of local variables when rendering a view to a rails 3 compatible format

---
+++ spec/views/pages/_editable_form.html.erb_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe 'pages/_editable_form.html.erb' do
+  before do
+
+    login_user(Factory(:faculty_frank))
+    assign(:page, Factory.build(:page))
+    assign(:courses, [
+      stub_model(Course),
+      stub_model(Course)
+    ])
+    view.should_receive(:button_name).any_number_of_times.and_return("Update")
+    render
+  end
+
+  it 'should have title fields' do
+      rendered.should have_selector('form') do |f|
+      f.should have_selector("input[name='page[title]']")
+      f.should have_selector("textarea[name='page[tab_one_contents]']")
+    end
+  end
+end
