85c7f3780238f933ee355644b0e2cb96a5b84ebf
Todd Sedano <todd.sedano@sv.cmu.edu>
2011-07-28 14:55:13 -0700

    Bug fix on person mailer

--- app/mailers/person_mailer.rb
+++ app/mailers/person_mailer.rb
@@ -3,17 +3,14 @@ class PersonMailer < ActionMailer::Base
           :cc => 'help@sv.cmu.edu',
           :bcc => 'todd.sedano@sv.cmu.edu',
           :subject => 'Welcome to Carnegie Mellon University Silicon Valley'
-          # Original default subject was - "Welcome to Carnegie Mellon University Silicon Valley (" + person.email + ")".
-          #TO ASK - Person's email required in the subject?
 
   def welcome_email(person, password, options = {})
     @person = person
     @password = password
 
-    # Original function signature - def welcome_email(welcome_email, person, password, options = {})
-    #TO ASK - What is passed in welcome_email, a param for recipients in original function
-
-    mail(:to => @person.email, :subject => options[:subject], :date => Time.now)
+    mail(:to => [@person.email, @person.personal_email],
+         :subject => options[:subject] || "Welcome to Carnegie Mellon University Silicon Valley (" + @person.email + ")",
+         :date => Time.now)
   end
 
 end
