8d0b6cdb166dab03e615aa1df13d092a7bcccb85
Todd Sedano <todd.sedano@sv.cmu.edu>
2011-09-13 14:32:41 -0700

    Refactored into is_completed_for? and added in a summary page for peer evaluations

---
+++ spec/models/peer_evaluation_review_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe PeerEvaluationReview do
+
+  before(:all) do
+      activate_authlogic
+  end
+
+  it 'is_completed_for? returns true for people who have done it' do
+    review = Factory(:peer_evaluation_review)
+    PeerEvaluationReview.is_completed_for?(review.author_id, review.team.id).should be_true
+  end
+
+  it 'is_completed_for? returns false for people who have not done it' do
+    PeerEvaluationReview.is_completed_for?(nil, nil).should be_false
+  end
+
+
+end
+
