93a2777ceff6486037c7173656f5f3cd87eff695
Todd Sedano <todd.sedano@sv.cmu.edu>
2011-09-16 07:36:24 -0700

    Updating rspec to ignore indextank

--- spec/models/page_model_spec.rb
+++ spec/models/page_model_spec.rb
@@ -10,6 +10,8 @@ describe Page do
     @page = Page.new(:title => "Syllabus",
                      :updated_by_user_id => 10,
                      :tab_one_contents => "As a student in this course, you have the opportunity to practice principled software development in the context of an authentic project using an agile method. You track your progress against a plan and manage risks along the way. You prioritize features, do pair programming and follow test-driven development. You measure code coverage and code quality. Through this course, you experience the ins and outs of software engineering.")
+    Page.any_instance.stub(:update_search_index)
+    Page.any_instance.stub(:delete_from_search)
   end
 
 
