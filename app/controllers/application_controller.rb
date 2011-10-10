25b5d3dbe3734628d1009578b2520687f9d70531
Todd Sedano <todd.sedano@sv.cmu.edu>
2011-10-10 16:58:54 -0700

    Fixed devise so that redirect after signin works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -50,15 +50,26 @@ class ApplicationController < ActionController::Base
       end
 
       #Sets up the return to page when logging a user in
-      def authenticate_user!
-        session[:return_to] = request.fullpath
-        super
+      #def authenticate_user!
+      #  session[:return_to] = request.fullpath
+      #  super
+      #end
+
+    def authenticate_user!
+      if !current_user
+        # This should work, but session is lost. See https://github.com/plataformatec/devise/issues/1357
+        # session[:return_to] = request.fullpath
+        redirect_to user_omniauth_authorize_path(:google_apps, :origin => request.fullpath)
       end
+    end
 
       #Return to the page the user was trying to access after a login
       def after_sign_in_path_for(resource)
-        stored_location_for(resource) || session[:return_to]
-        session[:return_to] = nil
+        # This should work, but session is lost. See https://github.com/plataformatec/devise/issues/1357
+        # return_to = session[:return_to]
+        # session[:return_to] = nil
+        return_to = request.env['omniauth.origin']
+        stored_location_for(resource) || return_to || root_path  
       end
 
       def store_location
