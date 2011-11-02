66f4d1bd06d42e94e6d6d4cf600f84237b9bae76
Todd Sedano <todd.sedano@sv.cmu.edu>
2011-11-02 08:44:34 -0700

    Added column that shows when faculty emailed out peer evaluation reports

--- app/models/peer_evaluation_report.rb
+++ app/models/peer_evaluation_report.rb
@@ -1,2 +1,8 @@
 class PeerEvaluationReport < ActiveRecord::Base
+
+  def self.emailed_on(team_id)
+    report = PeerEvaluationReport.find(:first, :conditions => {:team_id => team_id})
+    report.email_date unless report.nil?
+  end
+
 end
