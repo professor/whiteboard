92f84f4f602dcf4f2457a6831f7f1987e9a3e216
Todd Sedano <todd.sedano@sv.cmu.edu>
2011-11-05 20:25:48 -0700

    Bug fix: we never use xml data here

--- app/controllers/course_numbers_controller.rb
+++ app/controllers/course_numbers_controller.rb
@@ -3,16 +3,10 @@ class CourseNumbersController < ApplicationController
 
   before_filter :authenticate_user!
 
-
   # GET /course_numbers
   # GET /course_numbers.xml
   def index
     @courses = Course.unique_course_numbers_and_names_by_number
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml { render :xml => @course_numbers }
-    end
   end
 
 end
