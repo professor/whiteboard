3ee7b8c3a0002955eac21d1af30b44d4c237e199
Kang Chen <kang.chen@sv.cmu.edu>
2011-11-12 18:03:37 -0800

    added a couple initial specs on import_rtf

--- lib/cmusv/hub_student_importer/hub_student_importer.rb
+++ lib/cmusv/hub_student_importer/hub_student_importer.rb
@@ -1,7 +1,16 @@
+require 'spec_helper'
 require 'ruby-rtf'
 
 module HubStudentImporter
   extend self
 
-  
+  def import_rtf(src_file_path)
+    rtf_source = File.open(src_file_path, "r") { |f| f.read }
+    parser = RubyRTF::Parser.new
+    document = parser.parse(rtf_source)
+  end
+
+  def import_html(src_file_path)
+    
+  end
 end
