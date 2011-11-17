6f71e3bf30797060eedaa3cb46ddc69f207d59e1
Kang Chen <kang.chen@sv.cmu.edu>
2011-11-16 23:36:19 -0800

    Fixed logic errors and added more specs for RTF parser:
    - fixed issue where first and last name were swapped when saving a
      student's info
     - fixed escaping issue in sample RTF source code in spec
     - added specs to cover a successful #import_rtf invocation

--- lib/cmusv/hub_student_importer/hub_student_importer.rb
+++ lib/cmusv/hub_student_importer/hub_student_importer.rb
@@ -37,8 +37,8 @@ module HubStudentImporter
         elsif text.match(Course::META_INSTRUCTOR_NAME_MATCHER)
           course.instructors << $1
         elsif text.match(Student::META_STUDENT_INFO_MATCHER)
-            first_name, last_name = $1.split(", ")
-            course.students << Student.new({ :first_name => first_name.strip, :last_name => last_name.strip, :class => $2, :college => $3, :department => $4, :g_o => $5, :units => $6, :user_id => $7})
+            last_name, first_name = $1.split(", ")
+            course.students << Student.new({ :first_name => first_name.strip, :last_name => last_name.strip, :class => $2, :college => $3, :department => $4, :g_o => $5, :units => $6.to_f, :user_id => $7})
         elsif text.match(Course::META_TOTAL_STUDENTS_MATCHER)
           course.total_students = $1.to_i
         else
@@ -55,66 +55,66 @@ module HubStudentImporter
 
   #Similar logic flow for RTF parsing but for HTML instead
   def import_html(src_file_path)
-  	# Open file from src_file_path
-  	# create the HTML parser from Nokogiri
-  	# close the file stream
-     	html_file = File.open(src_file_path)
-	 html_parser = Nokogiri::HTML(html_file)
-	 html_file.close()
+    # Open file from src_file_path
+    # create the HTML parser from Nokogiri
+    # close the file stream
+      html_file = File.open(src_file_path)
+   html_parser = Nokogiri::HTML(html_file)
+   html_file.close()
 
-	 #define regular expression variables here;  These may be removed upon verification of logic used for RTF (leveraged RTF logic for HTML)
-	 html_line1_matcher = Regexp.new(/^Run Date: (\d{2}-\w+-\d{4})\s*Course:\s*(\d+)\s*Sect:\s*(\w+)\s*(.*)$/)
-	 html_line2_matcher = Regexp.new(/^Semester: (\w+)\s*College:\s*(\w+)\s*Department:\s*(\w+).*$/)
-	 html_instructor_line = Regexp.new(/^\s+Instructor\(s\): (.*)$/)
-	 html_instructor_name_line = Regexp.new(/(\S+, \S+)$/)
-	 html_meta_student_info = Regexp.new(/^(\w+\s?\w*, \w+\s?[a-zA-z|\.]*)\s+(\w+) (\w+)\s*(\w+)\s+(\w+)\s+(\d+\.\d)\s*(\w+).*$/)
-	 html_meta_total_students = Regexp.new(/^Total Number Of Students In Course.*is\s+(\d+)$/)
-	 
-	 courses = []
-	 course = Course.new
-	 
-	 #  string_array[10].match(/\w+\s?\w*, \w+\s?[a-zA-z|\.]*/).strip - for last name, first name
-	 
-	 # Parse the html file by the tag <pre> and loop per tag (the HTML contents that we care about are surrounded in <pre> tags.
-	 # In the case where a multi-set HTML document exists (e.g. Foundations has a A section and a B section), the <pre> tags easily
-	 # distinguish which Semester portion the students are registered for.
-	 
-	 search_parser = html_parser.search("//pre")
-	 search_parser.each do |search_section|
-	 
-	 # split the file by newline character to access data line by line
-	    file_array = search_section.to_s.split(/\n+/)
-	    file_array.each_with_index do |string_line, i|
-		
-			unless string_line.match("</pre>")
-				if string_line.match(html_line1_matcher)
-					course.run_date = Time.parse($1)
-					course.number = $2
-					course.section = $3
-					course.name = $4
-				elsif string_line.match(html_line2_matcher)
-					course.semester = $1
-					course.college = $2
-					course.department = $3
-				elsif string_line.match(html_instructor_line)
-					course.instructors << $1
-				elsif string_line.match(html_instructor_name_line)
-					course.instructors << $1
-				elsif string_line.match(html_meta_student_info)
-					last_name, first_name = $1.split(", ")
-					course.students << Student.new({ :first_name => first_name.strip, :last_name => last_name.strip, :class => $2, :college => $3, :department => $4, :g_o => $5, :units => $6, :user_id => $7})
-				elsif string_line.match(html_meta_total_students)
-					course.total_students = $1.to_i
-				else
-					# ignore junk
-				end
-			else
-				courses << course
-				course = Course.new
-			end
-	    end
-	 end
-	 courses
+   #define regular expression variables here;  These may be removed upon verification of logic used for RTF (leveraged RTF logic for HTML)
+   html_line1_matcher = Regexp.new(/^Run Date: (\d{2}-\w+-\d{4})\s*Course:\s*(\d+)\s*Sect:\s*(\w+)\s*(.*)$/)
+   html_line2_matcher = Regexp.new(/^Semester: (\w+)\s*College:\s*(\w+)\s*Department:\s*(\w+).*$/)
+   html_instructor_line = Regexp.new(/^\s+Instructor\(s\): (.*)$/)
+   html_instructor_name_line = Regexp.new(/(\S+, \S+)$/)
+   html_meta_student_info = Regexp.new(/^(\w+\s?\w*, \w+\s?[a-zA-z|\.]*)\s+(\w+) (\w+)\s*(\w+)\s+(\w+)\s+(\d+\.\d)\s*(\w+).*$/)
+   html_meta_total_students = Regexp.new(/^Total Number Of Students In Course.*is\s+(\d+)$/)
+   
+   courses = []
+   course = Course.new
+   
+   #  string_array[10].match(/\w+\s?\w*, \w+\s?[a-zA-z|\.]*/).strip - for last name, first name
+   
+   # Parse the html file by the tag <pre> and loop per tag (the HTML contents that we care about are surrounded in <pre> tags.
+   # In the case where a multi-set HTML document exists (e.g. Foundations has a A section and a B section), the <pre> tags easily
+   # distinguish which Semester portion the students are registered for.
+   
+   search_parser = html_parser.search("//pre")
+   search_parser.each do |search_section|
+   
+   # split the file by newline character to access data line by line
+      file_array = search_section.to_s.split(/\n+/)
+      file_array.each_with_index do |string_line, i|
+    
+      unless string_line.match("</pre>")
+        if string_line.match(html_line1_matcher)
+          course.run_date = Time.parse($1)
+          course.number = $2
+          course.section = $3
+          course.name = $4
+        elsif string_line.match(html_line2_matcher)
+          course.semester = $1
+          course.college = $2
+          course.department = $3
+        elsif string_line.match(html_instructor_line)
+          course.instructors << $1
+        elsif string_line.match(html_instructor_name_line)
+          course.instructors << $1
+        elsif string_line.match(html_meta_student_info)
+          last_name, first_name = $1.split(", ")
+          course.students << Student.new({ :first_name => first_name.strip, :last_name => last_name.strip, :class => $2, :college => $3, :department => $4, :g_o => $5, :units => $6, :user_id => $7})
+        elsif string_line.match(html_meta_total_students)
+          course.total_students = $1.to_i
+        else
+          # ignore junk
+        end
+      else
+        courses << course
+        course = Course.new
+      end
+      end
+   end
+   courses
   end
 
   private
