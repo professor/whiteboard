e8de93f643e414f4e394af153750a4ec1c034967
Todd Sedano <todd.sedano@sv.cmu.edu>
2012-02-22 19:21:48 -0800

    Trying to solve  A sender (Return-Path, Sender or From) required to send a message

--- app/mailers/course_mailer.rb
+++ app/mailers/course_mailer.rb
@@ -1,12 +1,14 @@
 class CourseMailer < ActionMailer::Base
-  default :from => "CMU-SV Official Communication <help@sv.cmu.edu>",
-          :bcc => "todd.sedano@sv.cmu.edu"
+  #default :from => "CMU-SV Official Communication <help@sv.cmu.edu>",
+  #        :bcc => "todd.sedano@sv.cmu.edu"
 
   def configure_course_faculty_email(course, options = {})
     @course = course
     
     unless course.is_configured?
       mail(:to => options[:to] || course.faculty.collect { |person| person.email },
+           :from => "CMU-SV Official Communication <help@sv.cmu.edu>",
+           :bcc => "todd.sedano@sv.cmu.edu",
            :subject => options[:subject] || "Please let us know about your course #{course.name} (#{course.semester} #{course.year})",
            :date => Time.now)
     end
@@ -16,6 +18,8 @@ class CourseMailer < ActionMailer::Base
     @course = course
 
     mail(:to => options[:to] || "help@sv.cmu.edu",
+         :from => "CMU-SV Official Communication <help@sv.cmu.edu>",
+         :bcc => "todd.sedano@sv.cmu.edu",
          :subject => "Faculty has configured a course #{course.semester} #{course.year} #{course.name}",
          :date => Time.now)
   end
