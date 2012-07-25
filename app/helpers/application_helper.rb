ce807c22c9682bc8e7db65e3ddac8d9acc8096bd
Todd Sedano <todd.sedano@sv.cmu.edu>
2012-07-24 22:07:50 -0700

    Allow a TD to have a width

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
 
   def sanitize_trusted(html)
-    return sanitize html, :tags => %w(table tbody tr td p br span pre img a h1 h2 h3 ul ol li em div strong), :attributes => %w(id href name class style)
+    return sanitize html, :tags => %w(table tbody tr td p br span pre img a h1 h2 h3 ul ol li em div strong), :attributes => %w(id href name class width style)
   end
 
 
