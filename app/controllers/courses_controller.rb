d4155eb171d212ea5c08062000740c2d7c6c2ed9
Kaushik Gopal <code@kaush.co>
2012-09-16 22:34:10 -0700

    adding a check to make sure semester is only within [Fall, Summer, Spring]; also taking care of case sensitivity by capitalizing  - issue #108

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -15,7 +15,8 @@ class CoursesController < ApplicationController
   def index_for_semester
     @all_courses = false
     (@semester, @year) = AcademicCalendar.semester_and_year(params[:semester])
-    if @semester.nil?
+    @semester=@semester.capitalize
+    if @semester.nil? || !(["Fall", "Summer", "Spring"].include?@semester)
       current_semester = "#{AcademicCalendar.current_semester()}#{Date.today.year}"
       flash[:notice] = "The requested url #{request.path} does not look like /courses/semester/#{current_semester} -- Thus we brought you to the current semester."
       redirect_to("/courses/semester/#{current_semester}")
