a6d4310e41beb5d6b1b49d2cf7b37392ee3f08ad
Edward Akoto <edward.akoto@sv.cmu.edu>
2012-10-03 11:33:04 -0700

    test for image loading

--- spec/requests/people_search_spec.rb
+++ spec/requests/people_search_spec.rb
@@ -5,10 +5,7 @@ describe "PeopleSearch" do
   before do
     visit('/people')
   end
-  it "people_table should be empty when search box is blank"
-  it "people_table should be populated as key words are inserted to the search box"
-  it "people_table should display less than ten rows by default"
-  it "people images should be fetched only for displayed rows"
-  it "all rows should be displayed when search button is clicked"
+
+
 
 end
