008ab14ba763d49abfa97086679c25ff7ba75659
clydeli <clyde1008li@gmail.com>
2012-10-03 13:42:47 -0700

    fix/update test cases for people_search page

--- spec/requests/people_search_spec.rb
+++ spec/requests/people_search_spec.rb
@@ -1,12 +1,6 @@
-require 'spec_helper'
+require "spec_helper"
 
-module AjaxWaiter
-  def wait_for_ajax
-    wait_until { page.evaluate_script("jQuery.active") == 0 }
-  end
-end
-
-describe "Initial_Search_Page" do
+describe "people search" do
 
   before do
     visit('/')
@@ -16,22 +10,20 @@ describe "Initial_Search_Page" do
   end
 
   it "Should have no results at the beginning" do
-    should_not have_selector '.data_card'
+    page.should have_selector('#results_box')
+    page.should_not have_selector('#results_box .data_card')
   end
 
-  context 'with results' do
+  context 'with search results' do
 
-    # I think this test is not needed anymore since '.data_card img' has implicitly required a ,data_card element to exist
     before do
       fill_in "qText" , :with => "Sam"
       click_button "submit_btn"
-      #wait_until { page.evaluate_script("jQuery.active") == 0 }
     end
 
-    it "Should display images with the search results" do
-      #wait_until(30) do
-        should have_selector '.data_card img'
-      #end
+    it "Should display images with the search results", :js => true do
+      wait_until { page.evaluate_script("jQuery.active") == 0 }
+      page.should have_selector('#results_box .data_card img')
     end
 
   end
