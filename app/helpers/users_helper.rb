5672530757e5200770c299995c958e03f4efb078
Todd Sedano <todd.sedano@sv.cmu.edu>
2012-10-04 10:45:10 -0700

    People are referenced by twiki name, not id, through urls. Removing twiki_user_link helper method from system

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,11 @@
 module UsersHelper
 
-  def twiki_user_link(twiki_username, human_name)
-    if twiki_username.blank?
-      "#{human_name}"
-    else
-      "<a href='http://rails.sv.cmu.edu/people/#{twiki_username}' target='_top'>#{human_name}</a>".html_safe
-    end
-  end
+  #def twiki_user_link(twiki_username, human_name)
+  #  if twiki_username.blank?
+  #    "#{human_name}"
+  #  else
+  #    "<a href='http://rails.sv.cmu.edu/people/#{twiki_username}' target='_top'>#{human_name}</a>".html_safe
+  #  end
+  #end
 
 end
