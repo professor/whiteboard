806e80d5e37a4c8badbc1f8bba9f10d52596e0be
David Liu <davidliu@David-Lius-MacBook-Pro.local>
2012-10-05 12:00:09 -0700

    grading range saving

--- spec/requests/courses_spec.rb
+++ spec/requests/courses_spec.rb
@@ -2,16 +2,15 @@ require "spec_helper"
 
 describe "courses" do
 
-   before do
-   visit('/')
-   @user = FactoryGirl.create(:student_sam)
-   @semester = AcademicCalendar.current_semester()
-   @year = Date.today.year
-   login_with_oauth @user
-   click_link "#{@semester} #{@year}" " Courses"
-  end
-
   context "current courses" do
+    before do
+      visit('/')
+      @user = FactoryGirl.create(:student_sam)
+      @semester = AcademicCalendar.current_semester()
+      @year = Date.today.year
+      login_with_oauth @user
+      click_link "#{@semester} #{@year}" " Courses"
+    end
 
     it "renders current courses page" do
       page.should have_content("#{@semester} #{@year}" " Courses")
@@ -28,16 +27,50 @@ describe "courses" do
       page.should have_selector("#courses_by_length")
     end
 
+    context "all courses" do
+      it 'renders all courses' do
+        click_link "Courses"
+        page.should have_content("All Courses")
+        page.should have_link("See current semester")
+      end
+    end
   end
 
-  context "all courses" do
-    it 'renders all courses' do
-      click_link "Courses"
-      page.should have_content("All Courses")
-      page.should have_link("See current semester")
-   end
+  context "configuring grading range" do
+    before do
+      visit('/')
+      @user = FactoryGirl.create(:faculty_frank_user)
+      @semester = AcademicCalendar.current_semester()
+      @year = Date.today.year
+      login_with_oauth @user
+      click_link "#{@semester} #{@year}" " Courses"
+      @course = FactoryGirl.create(:course)
+      visit configure_course_path(@course)
+    end
+
+    it "should reach the right course configuration page" do
+      page.should have_content("Configure your course, #{@course.name}")
+    end
+
+    it "should have grading range table" do
+      page.should have_selector("table#grading_range > tbody > tr", count: 5)
+      page.should have_selector("table#grading_range > tbody > tr:first > td", count: 2)
+    end
+
+    it 'should save when numerical values are entered' do
+      saved_grading_range = {}
+      JSON.parse(@course.grading_range).each_with_index do |(letter, value), index|
+        STDERR.puts letter.inspect, value.inspect, index.inspect
+        select letter, from: "grading_range_letter_#{index}"
+        STDERR.puts "hi madhok"
+        fill_in "grading_range_value_#{index}", with: value["minimum"] + 1
+        STDERR.puts "...."
+        saved_grading_range[letter] = {"minimum" => value["minimum"] + 1}
+        STDERR.puts "hi david"
+      end
+      click_button "Update"
+      @course.reload
+      @course.get_sorted_grading_range.should == ActiveSupport::JSON.encode(saved_grading_range)
+    end
   end
-
-
-
 end
