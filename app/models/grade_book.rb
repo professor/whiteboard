1a9455d4d5433bbf5d0401303e185027c5f92649
Kate Liu <kate@Kates-MacBook-Pro.local>
2012-10-12 12:59:01 -0700

    join gradebook with assignment

--- app/models/grade_book.rb
+++ app/models/grade_book.rb
@@ -2,9 +2,7 @@ class GradeBook < ActiveRecord::Base
   attr_accessible :course_id, :student_id, :assignment_id, :is_student_visible, :score
   belongs_to :course
   belongs_to :student, :class_name => "User"
-  #has_many :studnets, :source => :user
   belongs_to :assignment
-  #has_many :assignments
 
   def self.get_gradebooks (course, student)
     gradebooks = {}
