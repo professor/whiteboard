1a399e4c581121757b852df15a95d770e4cae91f
Kate Liu <kateycliu@gmail.com>
2012-10-13 17:14:15 -0700

    implemnt/test attribute validation for grade book mode

--- app/models/grade_book.rb
+++ app/models/grade_book.rb
@@ -1,23 +1,32 @@
+# GradeBook represents a grade given by the course instructor.
+# 
+# == Viewing grades
+# 
+# == Modifying grades
+#
 class GradeBook < ActiveRecord::Base
   attr_accessible :course_id, :student_id, :assignment_id, :is_student_visible, :score
   belongs_to :course
   belongs_to :student, :class_name => "User"
   belongs_to :assignment
+  validates :course_id, :student_id, :assignment_id, :presence => true 
+  validates :score, :numericality => {:greater_than_or_equal_to => 0} , :allow_nil => true, :allow_blank => true
+  validates :score, :uniqueness => {:scope => [:course_id, :assignment_id, :student_id]}
 
   def self.get_gradebooks (course, student)
-    gradebooks = {}
-    GradeBook.where(course_id: course.id).where(student_id: student.id).each do |gradebook|
-      gradebooks[gradebook.assignment.id] = gradebook 
+    grade_books = {}
+    GradeBook.where(course_id: course.id).where(student_id: student.id).each do |grade_book|
+      grade_books[grade_book.assignment.id] = grade_book
     end
-    gradebooks 
+    grade_books
   end
 
-  def self.get_grade_books(scoreValue)
-        grade_book = GradeBook.where(:course_id =>scoreValue["course_id"],:assignment_id => scoreValue["assignment_id"],:student_id => scoreValue["student_id"]).limit(1)[0]
+  def self.get_grade_books(score_value)
+    grade_book = GradeBook.where(:course_id =>score_value["course_id"],:assignment_id => score_value["assignment_id"],:student_id => score_value["student_id"]).limit(1)[0]
   end
-  def self.update_gradebook(courseAssignment)
-       GradeBook.update_all({:is_student_visible=>true},{:course_id=>courseAssignment["course_id"],:assignment_id=>courseAssignment["assignment_id"],:is_student_visible=>false})
 
+  def self.update_gradebook(course_assignment)
+    GradeBook.update_all({:is_student_visible=>true},{:course_id=>course_assignment["course_id"],:assignment_id=>course_assignment["assignment_id"],:is_student_visible=>false})
   end
 
 end
--- spec/models/grade_book_spec.rb
+++ spec/models/grade_book_spec.rb
@@ -1,60 +1,85 @@
 require 'spec_helper'
 
 describe GradeBook do
-  before(:all) do
-    @student_sam = FactoryGirl.build(:student_sam_user)
-    @course_fse = FactoryGirl.build(:course_fse_with_students, registered_students: [@student_sam])
-    @assignment_fse = FactoryGirl.build(:assignment_fse)
-    @grade_book = GradeBook.new(course: @course_fse, student: @student_sam, assignment: @assignment_fse)
-    #subject {GradeBook.new(course: @course_fse, student: @student_sam)}
+  before(:each) do
+    @student_sam = FactoryGirl.create(:student_sam_user)
+    @course_fse = FactoryGirl.create(:course_fse_with_students, registered_students: [@student_sam])
+    @assignment_fse = FactoryGirl.create(:assignment_fse)
+    @grade_book = GradeBook.create(:course_id => @course_fse.id, 
+                                   :student_id => @student_sam.id, 
+                                   :assignment_id => @assignment_fse.id,
+                                   :score => 0)
   end
 
-  it "should belong to a course" do
-    @grade_book.course.should == @course_fse
+  after(:each) do
+    @student_sam.delete
+    @course_fse.delete
+    @assignment_fse.delete
+    @grade_book.delete
   end
 
-  it "should belong to a student" do
-    @grade_book.student.should == @student_sam
+  subject { @grade_book }
+
+  context "course_id" do
+    its (:course_id) { should == @course_fse.id }
+    
+    it "should not be nil" do
+      subject.course_id = nil
+      subject.should_not be_valid
+    end
+
+    it "should not be blank" do
+      subject.course_id = ""
+      subject.should_not be_valid
+    end
+  end
+  
+  context "student_id" do
+    its (:student_id) {should == @student_sam.id}
+
+    it "should not be nil" do
+      subject.student_id = nil
+      subject.should_not be_valid
+    end
+
+    it "should not be blank" do
+      subject.student_id = ""
+      subject.should_not be_valid
+    end
   end
 
-  it "should belong to an assignment" do
-    @grade_book.assignment.should == @assignment_fse
+  context "assignment_id" do
+    its (:assignment_id) {should == @assignment_fse.id}
+    
+    it "should not be nil" do
+      subject.assignment_id = nil
+      subject.should_not be_valid
+    end
+
+    it "should not be blank" do
+      subject.assignment_id = ""
+      subject.should_not be_valid
+    end
   end
 
-  #its (:course) { should == @course_fse }
-  #its (:student) {should == @student_sam}
-  #its (:assignment) {should == @assignment_fse}
+  context "score" do
+    it "should not be string" do
+      subject.score = "just4test" 
+      subject.should_not be_valid
+    end  
 
-  #grade_book.scores  => {:assignment1=>1, :assignment2=>2 }
-  it "can get all students score"
+    it 'should not be negative' do
+      subject.score = -1.0 
+      subject.should_not be_valid
+    end
+  end
 
-  #grade_book.get_score(:assignment)
-  it "can get a student's specific assignment score"
-
-  #grade_book.student.name
-  it "can get a single student name"
-
-
-  #grade_book.get_score(:student) => {:course =>{:assignment1 =>1 ,...}}
-  it "can get a student's all score"
-
-  #grade_book.course.teams
-  it "can get all teams in a course"
-
-  # grade_book.student.team
-  it "can get a student team"
-
-  # grade_book.final_score
-  it "can get a student final score"
-
-  it "can get task number"
-  it "can get assignment name"
-  it "can get assignment score "
-  it "can get final score"
-
-
-
-
-
-
+  it 'should not be redundant' do
+    redundant_grade_book = GradeBook.new(
+      :course_id => @course_fse.id, 
+      :student_id => @student_sam.id, 
+      :assignment_id => @assignment_fse.id,
+      :score => 0)
+    redundant_grade_book.should_not be_valid
+  end
 end
