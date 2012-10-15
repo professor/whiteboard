bed69e5332ce034469955ef06b94167d721893e0
Owen Chu <owenchu@gmail.com>
2012-10-15 00:14:49 -0700

    Revert app/controllers/application_controller.rb and db/seeds.rb.

--- db/seeds.rb
+++ db/seeds.rb
@@ -139,18 +139,18 @@ FactoryGirl.define do
 
   end
 
-  factory :madhok, :parent => :person do
+  factory :your_name_here, :parent => :person do
     is_student 1
     is_part_time 0
-    graduation_year "2013"
+    graduation_year "2012"
     masters_program "SE"
     masters_track "Tech"
-    twiki_name "MadhokShivaratre"
-    first_name "Madhok"
-    last_name "Shivaratre"
-    human_name "Madhok Shivaratre"
-    email "Madhok.Shivaratre@sv.cmu.edu"
-    webiso_account "Madhok.Shivaratre@andrew.cmu.edu"
+    twiki_name "FirstLast"
+    first_name "First"
+    last_name "Last"
+    human_name "Your Name"
+    email "your.email@sv.cmu.edu"
+    webiso_account "your.name@andrew.cmu.edu"
   end
 
 end
@@ -163,7 +163,7 @@ Factory(:task_type, :name => "Other")
 
 todd = Factory.create(:todd)
 ed = Factory.create(:ed)
-Factory.create(:madhok)
+Factory.create(:your_name_here)
 Factory.create(:team_terrific) #This will create awe_smith, betty_ross, and charlie_moss
 
 
