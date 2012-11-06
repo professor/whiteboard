3ede502575d31b58f26f1104a659cd86cf7317df
Owen Chu <owenchu@gmail.com>
2012-11-06 01:48:47 -0800

    Update final grades in gradebook via AJAX.

--- app/controllers/course_user_grades_controller.rb
+++ app/controllers/course_user_grades_controller.rb
@@ -2,17 +2,18 @@ class CourseUserGradesController < ApplicationController
 
   def create
     @course = Course.find_by_id(params[:course_user_grade][:course_id])
-    course_user_grade = @course.course_user_grades.find_by_user_id(params[:course_user_grade][:user_id])
-    if course_user_grade.blank?
-      course_user_grade = CourseUserGrade.create(params[:course_user_grade])
+    @course_user_grade = @course.course_user_grades.find_by_user_id(params[:course_user_grade][:user_id])
+
+    if @course_user_grade.blank?
+      @course_user_grade = CourseUserGrade.create(params[:course_user_grade])
     else
-      course_user_grade.update_attributes(params[:course_user_grade])
+      @course_user_grade.update_attributes(params[:course_user_grade])
     end
-    if course_user_grade.save
-      flash[:notice] = 'Grade save successful'
-    else
-      flash[:error] = 'Unable to save grades'
+
+    @success = @course_user_grade.save
+
+    respond_to do |format|
+      format.js
     end
-    redirect_to course_gradebook_path(@course)
   end
 end
