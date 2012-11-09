c7e09701e7f9f5cdf2ea6bc38fd2ad487e717f03
ZhipengLi <charrli@126.com>
2012-11-09 12:11:32 -0800

    improvement made

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -94,27 +94,16 @@ class PeopleController < ApplicationController
   def download_csv
     @people = return_search_results(params[:filterBoxOne])
     respond_to do |format|
-      #format.json { render :json =>  @ppl, :layout => false }
       format.csv do
         csv_string = CSV.generate do |csv|
-          csv << ["First","Last","Email","Phone 1 - Type","Phone 1 - Value","Phone 2 - Type","Phone 2 - Value","Phone 3 - Type","Phone 3 - Value","Phone 4 - Type","Phone 4 - Value"]
-          #csv << ["First","Last","Email","Mobile","Work","Home"]
-          #phones_hash = user.telephones_hash
-
-          #phones = Hash.new
-
-
+          csv << ["Name","Given Name","Additional Name","Family Name","Yomi Name","Given Name Yomi","Additional Name Yomi","Family Name Yomi","Name Prefix","Name Suffix","Initials","Nickname","Short Name","Maiden Name","Birthday","Gender","Location","Billing Information","Directory Server","Mileage","Occupation","Hobby","Sensitivity","Priority","Subject","Notes","Group Membership","E-mail 1 - Type","E-mail 1 - Value","Phone 1 - Type","Phone 1 - Value","Phone 2 - Type","Phone 2 - Value","Phone 3 - Type","Phone 3 - Value","Phone 4 - Type","Phone 4 - Value"]
 
           @people.each do |user|
-
-            #csv <<[user.first_name,user.last_name,user.email, user.telephone1,user.telephone2,user.telephone3 ]
-            #csv <<[user.first_name,user.last_name,user.email, phone_array[0][:key],phone_array[0][:value],phone_array[1][:key],phone_array[1][:value],phone_array[2][:key],phone_array[2][:value],phone_array[3][:key],phone_array[3][:value] ]
-            csv <<[user.first_name,user.last_name,user.email, csv_name_converter(user.telephone1_label),user.telephone1,
-                   csv_name_converter(user.telephone2_label),user.telephone2,
-                   csv_name_converter(user.telephone3_label),user.telephone3,
-                   csv_name_converter(user.telephone4_label),user.telephone4]
-            #,phone_array[1][:key],phone_array[1][:value],phone_array[2][:key],phone_array[2][:value],phone_array[3][:key],phone_array[3][:value] ]
-
+            csv << [user.first_name,user.first_name,"",user.last_name,"","","","","","","","","","","","","","","","","","","","","","","","other",user.email,
+                csv_name_converter(user.telephone1_label),user.telephone1,
+                csv_name_converter(user.telephone2_label),user.telephone2,
+                csv_name_converter(user.telephone3_label),user.telephone3,
+                csv_name_converter(user.telephone4_label),user.telephone4]
           end
         end
 
@@ -128,9 +117,6 @@ class PeopleController < ApplicationController
   # GET /people/download_vcf
   def download_vcf
     @people = return_search_results(params[:filterBoxOne])
-    # respond_to do |format|
-    #format.json { render :json =>  @ppl, :layout => false }
-    #format.vcf do
 
     vcard_str=""
 
@@ -143,12 +129,8 @@ class PeopleController < ApplicationController
           name.family = user.last_name
         end
 
-        #phones_hash = user.telephones_hash.map { |k,v| "#{k}: #{v}" }
         phones_hash = user.telephones_hash
-
-
         maker.add_email(user.email)
-        #maker.add_tel('416 123 2222') { |t| t.location = 'home'; t.preferred = true }
         phones_hash.each do |k,v|
           maker.add_tel(v) do |tel|
             tel.location = "work" if k == "Work"
@@ -158,10 +140,6 @@ class PeopleController < ApplicationController
             tel.location = "voice" if k == "Google Voice"
           end
         end
-        #maker.add_tel(user.telephone2) if (!user.telephone2.blank?)
-        #maker.add_tel(user.telephone3) if (!user.telephone3.blank?)
-        #maker.add_tel(user.telephone4) if (!user.telephone4.blank?)
-
       end
 
       vcard_str<<card.to_s
@@ -171,8 +149,7 @@ class PeopleController < ApplicationController
     send_data vcard_str,
               :type=>"text/vcf; charset=utf-8",
               :disposition =>"attachment; filename=contact.vcf"
-    #end
-    #end
+
   end
 
   #Ajax call for autocomplete using params[:term]
@@ -581,6 +558,6 @@ class PeopleController < ApplicationController
   #    else
   #      return ""
   #  end
-  end
+  #end
 
 end
