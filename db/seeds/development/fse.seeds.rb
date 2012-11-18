12518edd84c8cbd1b50400151793cc3ffd7d255e
Kate Liu <kateycliu@gmail.com>
2012-11-18 09:40:52 -0800

    add deliverable attachment

--- db/seeds/development/fse.seeds.rb
+++ db/seeds/development/fse.seeds.rb
@@ -205,6 +205,7 @@ FactoryGirl.define do
       course.faculty << FactoryGirl.create(:prof_liu)
       course.faculty << FactoryGirl.create(:prof_singh)
       course.faculty << FactoryGirl.create(:prof_lee)
+
       course.teams = []
       course.teams << FactoryGirl.create(:team_3amigos)
       course.teams << FactoryGirl.create(:team_leopard)
@@ -237,10 +238,25 @@ course_fse.teams.each do |team|
 end
 
 course_fse.assignments.each do |assignment|
-  course_fse.registered_students.each do |student|
-    grade = FactoryGirl.create(:grade_points, :course_id=>course_fse.id, :assignment => assignment, :student_id => student.id)
-    grade.score = 1+Random.rand(assignment.maximum_score)
-    grade.save
+  if assignment.is_team_deliverable
+    course_fse.teams.each do |team|
+      deliverable=FactoryGirl.create(:team_deliverable_simple, :team_id=>team.id, :creator_id=>team.members.first.id, :course_id=>course_fse.id, :assignment_id=>assignment.id)
+      FactoryGirl.create(:deliverable_attachment, :deliverable_id=>deliverable.id, :submitter_id=>team.members.first.id, :attachment_file_name=>"#{team.members.first.human_name}_file", :submission_date=>Time.now)
+      score = 1+Random.rand(assignment.maximum_score)
+      team.members.each do |member|
+        grade = FactoryGirl.create(:grade_points, :course_id=>course_fse.id, :assignment => assignment, :student_id => member.id)
+        grade.score = score
+        grade.save
+      end
+    end
+  else
+    course_fse.registered_students.each do |student|
+      deliverable=FactoryGirl.create(:individual_deliverable, :creator_id=>student.id, :course_id=>course_fse.id, :assignment_id=>assignment.id)
+      FactoryGirl.create(:deliverable_attachment, :deliverable_id=>deliverable.id, :submitter_id=>student.id, :attachment_file_name=>"#{student.human_name}_file", :submission_date=>Time.now)
+      grade = FactoryGirl.create(:grade_points, :course_id=>course_fse.id, :assignment => assignment, :student_id => student.id)
+      grade.score = 1+Random.rand(assignment.maximum_score)
+      grade.save
+    end
   end
 end
 
