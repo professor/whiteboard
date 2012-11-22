f716d77038b8f90277e2f8ddb98ba8a5de71b669
Kate Liu <kateycliu@gmail.com>
2012-11-22 11:39:04 -0800

    remove curreent_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -50,10 +50,10 @@ class ApplicationController < ActionController::Base
   end
 
   # In development, if you want to pretend to be a different user, you can set it easily here
-  def current_user
-    User.last
-    @current_user = User.find_by_id(9)
-  end
+  #def current_user
+  #  User.last
+  #  @current_user = User.find_by_id(9)
+  #end
 
   def authenticate_user!
     if !current_user
