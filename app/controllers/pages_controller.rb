59eb36224bf5624fb18c6c755c6e549cae1bc899
Mike Vrooman <mike.vrooman@gmail.com>
2012-11-23 20:53:15 -0700

    Initial commit for multiple editors implementation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -89,10 +89,31 @@ class PagesController < ApplicationController
       redirect_to(page_url) and return
     end
 
+    now = Time.now
+    editing_started_minutes_ago = ((now - @page.updating_started_at) / 1.minute).round
+    if @page.updater_user_id.nil? ||
+        @page.updating_started_at.nil?||
+        editing_started_minutes_ago > 30 ||
+        @page.updater_user_id == current_user.id
+      @page.updater_user_id=current_user.id
+      @page.updating_started_at=now
+      @page.save!
+    else
+      flash[:notice] = "#{@page.current_updater.human_name} started editing this page
+                        #{pluralize(editing_started_minutes_ago, 'minute')} ago at
+                       '#{@page.updating_started_at.getlocal.strftime('%Y-%m-%d %I:%M:%S %p')}'"
+    end
+
+
     respond_to do |format|
       format.html # new.html.erb
       format.xml { render :xml => @page }
     end
+  end
+
+  def pluralize(number, text)
+    return number.to_s+' '+text.pluralize if number != 1
+    number.to_s+' '+text
   end
 
   # POST /pages
@@ -130,6 +151,12 @@ class PagesController < ApplicationController
     #@page.course = course
 
     @page.updated_by_user_id = current_user.id if current_user
+
+    #only reset the editor if the person saving the changes was the person that started updating
+    if @page.updated_by_user_id==@page.updater_user_id
+      @page.updater_user_id=nil
+    end
+
     respond_to do |format|
       if @page.update_attributes(params[:page])
         flash[:notice] = 'Page was successfully updated.'
