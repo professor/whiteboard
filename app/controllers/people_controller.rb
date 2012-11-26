56087c4feadc92d5eea487bb45d4c90ae9be78ce
Edward Akoto <edward.akoto@sv.cmu.edu>
2012-11-26 11:49:15 -0800

    map cmu sv email to work or other as appropriate

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -146,12 +146,13 @@ def download_csv
     respond_to do |format|
       format.csv do
         csv_string = CSV.generate do |csv|
-          csv << ["Name","Given Name","Additional Name","Family Name","Yomi Name","Given Name Yomi","Additional Name Yomi","Family Name Yomi","Name Prefix","Name Suffix","Initials","Nickname","Short Name","Maiden Name","Birthday","Gender","Location","Billing Information","Directory Server","Mileage","Occupation","Hobby","Sensitivity","Priority","Subject","Notes","Group Membership","E-mail 1 - Type","E-mail 1 - Value","Phone 1 - Type","Phone 1 - Value","Phone 2 - Type","Phone 2 - Value","Phone 3 - Type","Phone 3 - Value","Phone 4 - Type","Phone 4 - Value","Organization 1 - Type", "Organization 1 - Name", "Organization 1 - Yomi Name", "Organization 1 - Title", "Organization 1 - Department", "Organization 1 - Symbol", "Organization 1 - Location", "Organization 1 - Job Description"]
+          csv << ["Name","Given Name","Additional Name","Family Name","Yomi Name","Given Name Yomi","Additional Name Yomi","Family Name Yomi","Name Prefix","Name Suffix","Initials","Nickname","Short Name","Maiden Name","Birthday","Gender","Location","Billing Information","Directory Server","Mileage","Occupation","Hobby","Sensitivity","Priority","Subject","Notes","Group Membership","E-mail 1 - Type","E-mail 1 - Value","E-mail 2 - Type","E-mail 2 - Value","Phone 1 - Type","Phone 1 - Value","Phone 2 - Type","Phone 2 - Value","Phone 3 - Type","Phone 3 - Value","Phone 4 - Type","Phone 4 - Value","Organization 1 - Type", "Organization 1 - Name", "Organization 1 - Yomi Name", "Organization 1 - Title", "Organization 1 - Department", "Organization 1 - Symbol", "Organization 1 - Location", "Organization 1 - Job Description"]
 
           @people.each do |user|
             org = user.organization_name.nil? ? "" : user.organization_name
             title = user.title.nil? ? "" : user.title
-            csv << [user.first_name,user.first_name,"",user.last_name,"","","","","","","","","","","","","","","","","","","","","","","","other",user.email,
+            csv << [user.first_name,user.first_name,"",user.last_name,"","","","","","","","","","","","","","","","","","","","","","","",user.is_staff? ? "Work" : "Other",user.email,"Home",user.personal_email,
+
                 csv_name_converter(user.telephone1_label),user.telephone1,
                 csv_name_converter(user.telephone2_label),user.telephone2,
                 csv_name_converter(user.telephone3_label),user.telephone3,
@@ -183,7 +184,8 @@ def download_csv
         end
 
         phones_hash = user.telephones_hash
-        maker.add_email(user.email) { |e| e.location = 'work' }
+        maker.add_email(user.email) { |e| e.location = user.is_staff? ? 'work' : 'other' }
+        maker.add_email(user.personal_email) { |e| e.location = 'home' }
         maker.title = user.title unless user.title.nil?
         maker.org = user.organization_name unless user.organization_name.nil?
         phones_hash.each do |k,v|
