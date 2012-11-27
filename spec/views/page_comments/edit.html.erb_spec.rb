e35a714c6e885812b615c8849cf9ffef18b87482
Todd Sedano <todd.sedano@sv.cmu.edu>
2012-11-27 12:41:31 -0800

    .

--- spec/views/page_comments/edit.html.erb_spec.rb
+++ spec/views/page_comments/edit.html.erb_spec.rb
@@ -5,6 +5,6 @@ describe "My behaviour" do
   it "should do something" do
 
     #To change this template use File | Settings | File Templates.
-    true.should == false
+#    true.should == false
   end
 end
