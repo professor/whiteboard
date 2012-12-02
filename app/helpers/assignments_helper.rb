90fae594533e98eed842b1bd7bd56a65993ee4d9
Prabhjot Singh <prabhjot.singh@sv.cmu.edu>
2012-12-01 18:55:19 -0800

    customise the grade type

--- app/helpers/assignments_helper.rb
+++ app/helpers/assignments_helper.rb
@@ -7,4 +7,17 @@ module AssignmentsHelper
       return "Assignment"
     end
   end
+
+  def get_customised_grade_type
+    if @course.nil? || @course.grading_rule.nil?
+      return "Points"
+    else
+      if @course.grading_rule.grade_type=="weights"
+        return "Weight"
+      else
+        return "Points"
+        end
+    end
+  end
+
 end
