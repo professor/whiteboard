85dd51ced68bb27cd5dd2bcd8efe6884c148aa57
Todd Sedano <todd.sedano@sv.cmu.edu>
2013-01-20 13:46:08 -0800

    Look and feel improvements on team leopards integration of gradebook

--- app/controllers/deliverables_controller.rb
+++ app/controllers/deliverables_controller.rb
@@ -215,14 +215,13 @@ class DeliverablesController < ApplicationController
   end
 
   def edit_feedback
-    # Only staff can provide feedback
-    if !current_user.is_staff?
-      flash[:error] = "Only faculty can provide feedback on deliverables."
+    @deliverable = Deliverable.find(params[:id])
+
+    if !@deliverable.assignment.course.faculty.include?(current_user)
+      flash[:error] = "Only faculty teaching this course can provide feedback on deliverables."
       redirect_to :controller => "welcome", :action => "index"
       return
     end
-
-    @deliverable = Deliverable.find(params[:id])
   end
 
 
