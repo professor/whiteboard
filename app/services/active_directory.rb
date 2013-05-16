1d22dd37d06c9559a2c48e3f1fe18ad08f058b38
Edward Akoto <edward.akoto@sv.cmu.edu>
2013-05-16 14:42:34 -0700

    completed create account service method

--- app/services/active_directory.rb
+++ app/services/active_directory.rb
@@ -4,12 +4,18 @@ require 'net/ldap'
 
 class ActiveDirectory
 
+  # Initialize connection to active directory
   def self.initialize
     @connection = Net::LDAP.new(:host=>LDAPConfig.host, :port =>LDAPConfig.port)
     @connection.encryption(:method=>:simple_tls) unless !LDAPConfig.is_encrypted?
     @connection.auth LDAPConfig.username, LDAPConfig.password unless LDAPConfig.username.nil? || LDAPConfig.password.nil?
   end
 
+  # Create user account in active directory
   def create_account(user)
+    @connection.add(:dn=>user.ldap_distinguished_name, :attributes=>user.ldap_attributes)
+    result = @connection.get_operation_result
+    logger.debug(result)
+    return result
   end
 end
