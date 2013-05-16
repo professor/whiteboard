f50bcd7f9ab7dbf89f7eabc55bde2593c8f9654c
Edward Akoto <edward.akoto@sv.cmu.edu>
2013-05-16 15:01:10 -0700

    added ldap helper methods

--- app/services/active_directory.rb
+++ app/services/active_directory.rb
@@ -13,9 +13,44 @@ class ActiveDirectory
 
   # Create user account in active directory
   def create_account(user)
-    @connection.add(:dn=>user.ldap_distinguished_name, :attributes=>user.ldap_attributes)
+    @connection.add(:dn=>user.ldap_distinguished_name(user), :attributes=>ldap_attributes(user))
     result = @connection.get_operation_result
     logger.debug(result)
     return result
   end
+
+  # Build account attributes
+  protected
+  def ldap_attributes(user)
+    attributes = {
+        :cn => user.human_name,
+        :objectclass => ["top", "person", "organizationalPerson", "user"],
+        :sn => user.last_name,
+        :givenName => user.first_name,
+        :displayName => user.human_name,
+        :userPrincipalName =>user.email,
+        :mail => user.email
+    }
+    return attributes
+  end
+
+  # Build user distinguished name for active directory account
+  def ldap_distinguished_name(user)
+    distinguished_name = "cn=#{user.human_name},"
+    base_distinguished_name = "dc=cmusv,dc=sv,dc=cmu,dc=local"
+
+
+    if user.is_staff
+      distinguished_name+="ou=Staff,ou=Sync,"
+    elsif !user.masters_program.blank?
+      distinguished_name+= "ou="+user.masters_program+",ou=Students,ou=Sync,"
+    else
+      distinguished_name+="ou=Sync,"
+    end
+
+    distinguished_name+=base_distinguished_name
+    logger.debug(distinguished_name)
+    return distinguished_name
+  end
+
 end
