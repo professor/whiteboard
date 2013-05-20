b49304ed5904cc61c03ee864df80dd33e2d9cf56
Todd Sedano <todd.sedano@sv.cmu.edu>
2013-05-19 23:24:06 -0700

    Sync schema.rb with production, not sure how courses_users, projects, and project_types found their way back into schema.rb, checkout out why there is a development_structure.sql

--- db/schema.rb
+++ db/schema.rb
@@ -69,6 +69,11 @@ ActiveRecord::Schema.define(:version => 20130520033431) do
   add_index "courses", ["twiki_url"], :name => "index_courses_on_twiki_url"
   add_index "courses", ["year"], :name => "index_courses_on_year"
 
+  create_table "courses_users", :id => false, :force => true do |t|
+    t.integer "course_id"
+    t.integer "user_id"
+  end
+
   create_table "curriculum_comment_types", :force => true do |t|
     t.string   "name"
     t.string   "background_color"
@@ -216,6 +221,31 @@ ActiveRecord::Schema.define(:version => 20130520033431) do
   end
 
   add_index "grading_rules", ["course_id"], :name => "index_grading_rules_on_course_id"
+
+  create_table "individual_contribution_for_courses", :force => true do |t|
+    t.integer "individual_contribution_id"
+    t.integer "course_id"
+    t.text    "answer1"
+    t.float   "answer2"
+    t.text    "answer3"
+    t.text    "answer4"
+    t.text    "answer5"
+  end
+
+  add_index "individual_contribution_for_courses", ["course_id"], :name => "index_individual_contribution_for_courses_on_course_id"
+  add_index "individual_contribution_for_courses", ["individual_contribution_id"], :name => "individual_contribution_for_courses_icid"
+
+  create_table "individual_contributions", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "year"
+    t.integer  "week_number"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "individual_contributions", ["user_id"], :name => "index_individual_contributions_on_user_id"
+  add_index "individual_contributions", ["week_number"], :name => "index_individual_contributions_on_week_number"
+  add_index "individual_contributions", ["year"], :name => "index_individual_contributions_on_year"
 
   create_table "page_attachments", :force => true do |t|
     t.integer  "page_id"
@@ -387,6 +417,26 @@ ActiveRecord::Schema.define(:version => 20130520033431) do
   add_index "presentations", ["presentation_date"], :name => "index_presentations_on_presentation_date"
   add_index "presentations", ["team_id"], :name => "index_presentations_on_team_id"
   add_index "presentations", ["user_id"], :name => "index_presentations_on_user_id"
+
+  create_table "project_types", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "project_types", ["name"], :name => "index_project_types_on_name"
+
+  create_table "projects", :force => true do |t|
+    t.string   "name"
+    t.integer  "project_type_id"
+    t.integer  "course_id"
+    t.boolean  "is_closed"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "projects", ["name"], :name => "index_projects_on_name"
 
   create_table "registrations", :id => false, :force => true do |t|
     t.integer  "course_id",  :null => false
