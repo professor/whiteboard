208dc1efaf2bfd1a437431c8f26a782ab97e0d10
Edward Akoto <edward.akoto@sv.cmu.edu>
2013-06-10 12:45:12 -0700

    remove active_directory_created from schema

--- db/schema.rb
+++ db/schema.rb
@@ -651,7 +651,6 @@ ActiveRecord::Schema.define(:version => 20130521194045) do
     t.string   "google_plus"
     t.datetime "people_search_first_accessed_at"
     t.boolean  "is_profile_valid"
-    t.datetime "active_directory_account_created"
     t.string   "auth_token"
     t.string   "password_reset_token"
     t.datetime "password_reset_sent_at"
