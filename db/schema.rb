171a7bd601bbdbb732135da5e5d59b697eadc9b6
Edward Akoto <edward.akoto@sv.cmu.edu>
2013-06-13 09:34:51 -0700

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130521171512) do
+ActiveRecord::Schema.define(:version => 20130521194045) do
 
   create_table "assignments", :force => true do |t|
     t.string   "name"
