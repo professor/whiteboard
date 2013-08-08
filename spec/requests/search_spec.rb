10ef961c8e6a3cba7ecafc24fde5b13ed1f0362f
Todd Sedano <todd.sedano@sv.cmu.edu>
2013-08-08 13:43:05 -0700

    Ignore index tank searches for travis

--- spec/requests/search_spec.rb
+++ spec/requests/search_spec.rb
@@ -13,20 +13,20 @@ describe "Search" do
       current_path.should == search_index_path
     end
 
-    it "valid string - page should show the results" do
+    it "valid string - page should show the results", :skip_on_build_machine => true do
       fill_in "query", :with => "Search String"
       click_button('searchSubmit')
       page.should have_content('Your search for "Search String" returned 0 results')
     end
 
     describe "invalid string" do
-      it "like '-+_' - the page should not tank" do
+      it "like '-+_' - the page should not tank", :skip_on_build_machine => true do
         fill_in "query", :with => "-+_"
         click_button('searchSubmit')
         current_path.should == search_index_path
         page.should have_content('Search for: ')
       end
-      it "like '-%20%3C/p%3E%20-' - the page should not tank" do
+      it "like '-%20%3C/p%3E%20-' - the page should not tank", :skip_on_build_machine => true do
         fill_in "query", :with => "-%20%3C/p%3E%20-"
         click_button('searchSubmit')
         page.should have_content('Your search for "-%20%3C/p%3E%20-" returned 0 results')
