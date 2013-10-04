c77356165893f6d230e346a3cbafd62f38ae0a32
Todd Sedano <todd.sedano@sv.cmu.edu>
2013-10-04 16:38:05 -0700

    Removing 255 limitation on objective

--- spec/models/peer_evaluation_learning_objective_spec.rb
+++ spec/models/peer_evaluation_learning_objective_spec.rb
@@ -8,26 +8,4 @@ describe PeerEvaluationLearningObjective do
     }.should change(PeerEvaluationLearningObjective, :count).by(1)
   end
 
- context "is not valid" do
-
-    # we now allow nil as a valid value
-    # [:learning_objective].each do |attr|
-    #   it "without #{attr}" do
-    #     subject.should_not be_valid
-    #     subject.errors[attr].should_not be_empty
-    #   end
-    # end
-  
-    it 'with a learning objective longer than 255 characters' do
-      pelo = FactoryGirl.build(:peer_evaluation_learning_objective)
-      new_learning_objective = ""
-      256.times do |i|
-        new_learning_objective += "1"
-      end
-      pelo.learning_objective = new_learning_objective
-      pelo.save
-      pelo.should_not be_valid
-      pelo.errors[:learning_objective].should_not be_empty
-    end
-  end
 end
