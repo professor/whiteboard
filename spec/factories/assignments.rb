e2d0bc6d82ff9ddf166392748da574626a283268
nelson pollard <nelson.pollard@sv.cmu.edu>
2013-10-22 18:10:30 -0700

    Have a partially written rspec test for sorting by task number Currently just populates the grading queue with a single submission

--- spec/factories/assignments.rb
+++ spec/factories/assignments.rb
@@ -34,25 +34,18 @@ FactoryGirl.define do
     association :course, :factory => :fse
   end
 
-  factory :assignment1_fse, :parent=>:assignment do
-    name "assignment 1"
-    task_number 1
-    is_team_deliverable false
-    association :course, :factory=>:fse_fall_2011
-  end
-
-  factory :assignment2_fse, :parent=>:assignment do
-    name "assignment 2"
+  factory :assignment_fse_individual2, :parent=>:assignment do
+    name "fse assignment individual 2"
     task_number 2
     is_team_deliverable false
-    association :course, :factory=>:fse_fall_2011
+    association :course, :factory => :fse
   end
 
-  factory :assignment3_fse, :parent=>:assignment do
-    name "assignment 3"
+  factory :assignment_fse_individual3, :parent=>:assignment do
+    name "fse assignment individual 3"
     task_number 3
     is_team_deliverable false
-    association :course, :factory=>:fse_fall_2011
+    association :course, :factory => :fse
   end
 
 
