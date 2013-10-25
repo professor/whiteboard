050658e3d87e3eb2f1e6e9ba111978ff4a3ddf88
nelson pollard <nelson.pollard@sv.cmu.edu>
2013-10-24 17:57:38 -0700

    Removed attr_accessible from our mass assignment models

--- app/models/peer_evaluation_review.rb
+++ app/models/peer_evaluation_review.rb
@@ -32,8 +32,6 @@ class PeerEvaluationReview < ActiveRecord::Base
   belongs_to :author, :class_name => "User"
   belongs_to :recipient, :class_name => "User"
 
-  attr_accessible :answer
-
   def self.is_completed_for?(user_id, team_id)
     !PeerEvaluationReview.where({:team_id => team_id, :author_id => user_id}).empty?
   end
