99894787a74e0c6e3d7109a9278a0f218e2586a3
Isil Demir <isil.demir@sv.cmu.edu>
2013-11-12 22:36:22 -0800

    AJAX requests are done 'onclick' rather than using the submit button

--- app/controllers/deliverables_controller.rb
+++ app/controllers/deliverables_controller.rb
@@ -84,7 +84,7 @@ class DeliverablesController < ApplicationController
     end
 
     # Filter by assignment names in drop down menu
-    unless params[:filter_options][:assignment_id].empty?
+    unless params[:filter_options][:assignment_id].empty? or params[:filter_options][:assignment_id] == '-1'
       @deliverables = @deliverables.select{ |deliverable| deliverable.assignment_id == params[:filter_options][:assignment_id].to_i }
     end
 
