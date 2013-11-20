2d5cd246f853acfc3bf85ce44647b648c161005b
Rofaida Abdelaal <rofaida.awad@gmail.com>
2013-11-20 12:06:02 -0800

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,8 @@ CMUEducation::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  #config.action_controller.asset_host = "http://cmusv-rails-assets-production.s3.amazonaws.com/assets"
+  config.action_controller.asset_host = "d1z5n3u3tyi6h3.cloudfront.net"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
