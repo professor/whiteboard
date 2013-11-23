57dac3a9ffe8cf6f619e4c7aa63be86121e205a3
nelson pollard <nelson.pollard@sv.cmu.edu>
2013-11-22 22:58:48 -0800

    Implemented Team Ruby Racer's time zone bug fix

--- spec/lib/reminder_handler_spec.rb
+++ spec/lib/reminder_handler_spec.rb
@@ -8,11 +8,16 @@ describe ReminderHandler do
       before :each do
         @current_time = Time.now
         @user = FactoryGirl.build_stubbed(:faculty_frank_user)
+
+         # While storing to Active Record, use current_time.utc as Active record
+         # assumes pacific time (config/application.rb) but stores as utc
+         # internally. Causing the test case to fail.
+         # Time zone overriden in (config/environments/test.rb)
         @page1 = FactoryGirl.build_stubbed(:ppm, updated_by_user_id: @user.id,
-                                          updated_at: @current_time - 1.year)
+                                          updated_at: @current_time.utc - 1.year)
         @page2 = FactoryGirl.build_stubbed(:ppm, url: "page2",
                                           updated_by_user_id: @user.id,
-                                          updated_at: @current_time - 13.months)
+                                          updated_at: @current_time.utc - 13.months)
         Page.stub(:all).and_return([@page1, @page2])
         @page1.stub(:updated_by).and_return(@user)
         @page2.stub(:updated_by).and_return(@user)
