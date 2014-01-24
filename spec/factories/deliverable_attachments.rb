a1a25ade4d9d210770d2476ddabcdbd1db7663e0
Todd Sedano <todd.sedano@sv.cmu.edu>
2014-01-24 10:41:40 -0800

    Integrating Team Turing code

---
+++ spec/factories/deliverable_attachments.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+
+  ## beg add turing
+
+  factory :attachment_1, :parent => :deliverable_attachment do
+    submitter_id 1
+    deliverable_id 1
+    submission_date DateTime.now
+    association :deliverable, :factory => :team_deliverable
+    association :submitter, :factory => :student_john_user
+  end
+
+  ## end add turing
+end
