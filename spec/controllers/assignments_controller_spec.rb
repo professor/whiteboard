2fc555045c8ffeb796cb16b6433807acc578c9cf
Joe Mirizio <joe@joemirizio.com>
2014-05-04 00:48:09 -0400

    Add test for reordering assignments through controller

--- spec/controllers/assignments_controller_spec.rb
+++ spec/controllers/assignments_controller_spec.rb
@@ -159,4 +159,23 @@ describe AssignmentsController do
     end
   end
 
+  describe "POST reposition" do
+    it "reorders the assignments" do
+      FactoryGirl.create(:assignment, course: @course)
+      FactoryGirl.create(:assignment, course: @course)
+      
+      # Generate an array of the assignment ids in order and reversed
+      assignment_ids = @course.assignments.map { |assignment| assignment.id }
+      reversed_assignment_ids = assignment_ids.reverse
+      
+      # POST the reversed ids and reload the course to obtain updated assignment order
+      post :reposition, :course_id => @course, :assignment => reversed_assignment_ids
+      @course.reload
+      updated_assignment_ids = @course.assignments.map { |assignment| assignment.id }
+
+      updated_assignment_ids.should_not eq(assignment_ids)
+      updated_assignment_ids.should eq(reversed_assignment_ids)
+    end
+  end
+
 end
